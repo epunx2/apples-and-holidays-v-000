5db3ca6ccc8265957402685ef45921665914ad26
Eric Chrobak <eric@ericchrobak.com>
2018-05-09 00:29:18 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{seasons}:"
     data.each do |holiday, supply|
       holidays = holiday.to_s.capitalize
-      puts "  #{holidays}: "
+      puts "\t #{holidays}: "
       supply.each do |info|
         supply_info = info.to_s.capitalize
         if i == 0
